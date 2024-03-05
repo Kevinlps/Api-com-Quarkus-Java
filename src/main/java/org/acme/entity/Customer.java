e8e8369fc519e3507f6663d84240fd4bcdb23bca
Kevinlps <kevinlopes3012@gmail.com>
2024-03-05 14:50:37 -0400

    finalized

---
+++ src/main/java/org/acme/entity/Customer.java
@@ -0,0 +1,22 @@
+package org.acme.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private Integer age;
+
+}
